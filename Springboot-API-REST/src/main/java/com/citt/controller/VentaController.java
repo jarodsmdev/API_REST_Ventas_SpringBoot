60f56727c5b4144a439d1111ead30424ac06c131
Jarod Smith <lbriones.dev@gmail.com>
2025-04-13 22:07:52 -0400

    Feat/Agrega Swagger-ui v2.7

--- Springboot-API-REST/src/main/java/com/citt/controller/VentaController.java
+++ Springboot-API-REST/src/main/java/com/citt/controller/VentaController.java
@@ -3,6 +3,8 @@ package com.citt.controller;
 import com.citt.exceptions.VentaNotFoundException;
 import com.citt.persistence.entity.Venta;
 import com.citt.persistence.services.VentaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,13 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/ventas")
+@Tag(name = "Venta", description = "Controlador para gestionar ventas")
 public class VentaController {
 
     @Autowired
     private VentaService ventaService;
 
+    @Operation(summary = "Crear una nueva venta", description = "Crea una nueva venta en el sistema")
     @PostMapping
     public ResponseEntity<Venta> crearVenta(@Valid @RequestBody Venta venta){
         URI location = ServletUriComponentsBuilder
@@ -32,23 +36,27 @@ public class VentaController {
     }
 
     @PutMapping("/{idVenta}")
+    @Operation(summary = "Actualizar una venta existente", description = "Actualiza los detalles de una venta existente")
     public ResponseEntity<Venta> actualizarVenta(@Valid @PathVariable Long idVenta, @RequestBody Venta venta) throws VentaNotFoundException {
         Venta ventaActualizada = ventaService.updateVenta(idVenta, venta);
         return ResponseEntity.ok(ventaActualizada);
     }
 
     @GetMapping
+    @Operation(summary = "Obtener todas las ventas", description = "Devuelve una lista de todas las ventas")
     public ResponseEntity<List<Venta>> getVentas(){
         return ResponseEntity.ok(ventaService.findAllVentas());
     }
 
     @GetMapping("/{idVenta}")
+    @Operation(summary = "Obtener una venta por ID", description = "Devuelve los detalles de una venta específica")
     public ResponseEntity<Venta> obtenerVenta(@PathVariable Long idVenta) throws VentaNotFoundException {
         Venta venta = ventaService.findById(idVenta);
         return ResponseEntity.ok(venta); // Retornamos la venta encontrada con un estado 200 (OK)
     }
 
     @DeleteMapping("/{idVenta}")
+    @Operation(summary = "Eliminar una venta", description = "Elimina una venta del sistema")
     public ResponseEntity<Void> eliminarVenta(@PathVariable Long idVenta) throws VentaNotFoundException {
         ventaService.deleteVenta(idVenta);
         return ResponseEntity.noContent().build(); // Respuesta 204 No Content si se elimina correctamente
